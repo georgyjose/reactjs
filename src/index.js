3b592c696bba2338545e6bfb6cee5ef1a926d24d
Georgy Jose <georgyjose@Georgys-MBP.Home>
2018-06-08 01:18:19 +0530

    Fixed git file replacement issue

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+
+function Ab(props){
+return (
+  <div class='myclass'>
+    <p id='n'>Name: {props.name}</p>
+    <p>Age: {props.age}</p>
+  </div>
+);
+}
+var myvar=(<div>
+<Ab name='Geo' age='21'/>
+<Ab name='Bob' age='21'/>
+</div>);
+
+ReactDOM.render(
+  myvar,
+  document.getElementById('root')
+);
